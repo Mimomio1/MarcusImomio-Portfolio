6fe57d78e875e67ad783ff4d30685ccb752e81c4
unknown <imomiom627@gmail.com>
2023-12-13 00:06:48 -0500

    Auto-generated commit [ci skip]

--- README.md
+++
@@ -1,3 +0,0 @@
-# MarcusImomioPortfolio
-
-This is my portfolio website with all the projects, work experience, etc.
--- src/app/model/message.model.ts
+++
@@ -1,7 +0,0 @@
-// message.model.ts
-export class Message {
-    name: string = '';
-    email: string = '';
-    subject: string = '';
-    content: string = '';
-  }
